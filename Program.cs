e26dd91f114f7fc892a79445eaf63ffbc257ae20
Lucas Teles <lucas.teles@lambda3.com.br>
2019-10-14 21:44:25 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿#region Using Statements
+using System;
+#endregion
+
+namespace Pong
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
